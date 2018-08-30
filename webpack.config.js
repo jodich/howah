648aa6c4529cc2ef9a735f722dccd2d12bee3178
jodich <jodi_choo@hotmail.com>
2018-08-30 22:34:23 +0800

    indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,38 @@ const path = require("path")
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
-  entry: {
-    main: './src/client/index.jsx'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
-  },
-  target: 'web',
-  devtool: '#source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.s?css$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      }
+    entry: {
+        main: './src/client/index.jsx'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].js'
+    },
+    target: 'web',
+    devtool: '#source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']å
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html",
+            excludeChunks: ['server']
+        })
     ]
-  },
-  resolve: {
-      extensions: ['*', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
 }
 
